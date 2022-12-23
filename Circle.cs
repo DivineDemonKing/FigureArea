630cae19730a90964fe1341e0b1241f5a65f3b9b
Георгий <@DESKTOP-IV0D95M>
2022-12-23 12:40:03 +0300

    Добавьте файлы проекта.

---
+++ Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureArea
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+        public double Circumference { get; set; }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureArea
+{
+    internal class Rectangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public double D { get; set; }
+    }
+}
